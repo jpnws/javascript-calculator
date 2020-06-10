c6e0a65c882e801571d1b37b5110726847d0ac04
Ji Park <61026408+jiparkdev@users.noreply.github.com>
2020-06-09 21:28:42 -0700

    Add the App component.

---
+++ index.js
@@ -0,0 +1,13 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("javascript-calculator"));
