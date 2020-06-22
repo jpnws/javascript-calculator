42bfd034241b8fb0d35c2dfe781c57a1aa8f2325
Ji Park <61026408+jiparkdev@users.noreply.github.com>
2020-06-21 22:23:16 -0700

    Apply some styles to the calculator.

--- index.js
+++ index.js
@@ -86,58 +86,62 @@ class App extends React.Component {
   render() {
     return (
       <div className="calc-container">
-        <button id="zero" onClick={this.handleClick}>
-          0
-        </button>
-        <button id="one" onClick={this.handleClick}>
-          1
-        </button>
-        <button id="two" onClick={this.handleClick}>
-          2
-        </button>
-        <button id="three" onClick={this.handleClick}>
-          3
-        </button>
-        <button id="four" onClick={this.handleClick}>
-          4
-        </button>
-        <button id="five" onClick={this.handleClick}>
-          5
-        </button>
-        <button id="six" onClick={this.handleClick}>
-          6
-        </button>
-        <button id="seven" onClick={this.handleClick}>
-          7
-        </button>
-        <button id="eight" onClick={this.handleClick}>
-          8
-        </button>
-        <button id="nine" onClick={this.handleClick}>
-          9
-        </button>
-        <button id="add" onClick={this.handleClick}>
-          +
-        </button>
-        <button id="subtract" onClick={this.handleClick}>
-          -
-        </button>
-        <button id="multiply" onClick={this.handleClick}>
-          *
-        </button>
-        <button id="divide" onClick={this.handleClick}>
-          /
-        </button>
-        <button id="equals" onClick={this.handleClick}>
-          =
-        </button>
-        <button id="decimal" onClick={this.handleClick}>
-          .
-        </button>
-        <button id="clear" onClick={this.handleClick}>
-          clear
-        </button>
-        <div id="display">{this.state.displayState}</div>
+        <div className="display-box" id="display">
+          {this.state.displayState}
+        </div>
+        <div className="calc-pad">
+          <button className="num-btn" id="zero" onClick={this.handleClick}>
+            0
+          </button>
+          <button className="num-btn" id="one" onClick={this.handleClick}>
+            1
+          </button>
+          <button className="num-btn" id="two" onClick={this.handleClick}>
+            2
+          </button>
+          <button className="num-btn" id="three" onClick={this.handleClick}>
+            3
+          </button>
+          <button className="num-btn" id="four" onClick={this.handleClick}>
+            4
+          </button>
+          <button className="num-btn" id="five" onClick={this.handleClick}>
+            5
+          </button>
+          <button className="num-btn" id="six" onClick={this.handleClick}>
+            6
+          </button>
+          <button className="num-btn" id="seven" onClick={this.handleClick}>
+            7
+          </button>
+          <button className="num-btn" id="eight" onClick={this.handleClick}>
+            8
+          </button>
+          <button className="num-btn" id="nine" onClick={this.handleClick}>
+            9
+          </button>
+          <button className="op-btn" id="add" onClick={this.handleClick}>
+            +
+          </button>
+          <button className="op-btn" id="subtract" onClick={this.handleClick}>
+            -
+          </button>
+          <button className="op-btn" id="multiply" onClick={this.handleClick}>
+            *
+          </button>
+          <button className="op-btn" id="divide" onClick={this.handleClick}>
+            /
+          </button>
+          <button className="dot-btn" id="decimal" onClick={this.handleClick}>
+            .
+          </button>
+          <button className="clr-btn" id="clear" onClick={this.handleClick}>
+            Clear
+          </button>
+          <button className="eval-btn" id="equals" onClick={this.handleClick}>
+            =
+          </button>
+        </div>
       </div>
     );
   }
